98755cb32e165922a728060f76bff427aec9e1cc
BAI <yongyinbai0@gmail.com>
2025-06-04 13:37:01 +0200

    minisito

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 
 def home(request):
+
+    testo = request.POST.get('testo')
+    numero = request.POST.get('numero')
+
     context = {
-        'messaggio': 'Benvenuto nella homepage!'
+        'messaggio': 'Benvenuto nella homepage!',
+        'testo':testo,
+        'numero':numero
     }
 
-    return render(request, 'home.html', context)
+    return render(request, 'indice.html', context)
+
+def indice(request):
+    return render(request, 'indice.html')
+def musica(request):
+    return render(request, 'musica.html')
+def film(request):
+    return render(request, 'film.html')
